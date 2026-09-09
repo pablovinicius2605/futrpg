44c08b10dd3017982a9b90a58f4c99adf0e6014b
pablovinicius2605 <pablovinicius2605@gmail.com>
2026-09-09 11:31:50 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-registrarRotasCopa(app);
+
 
 const app = express();
 const server = http.createServer(app);
